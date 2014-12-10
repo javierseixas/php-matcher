9d4faf561e91d403a8f1b828fe79c8d3630d7965
Javier Seixas <javier.seixas@groupalia.com>
2014-12-10 14:17:12 +0100

    Implements @ignore_extra_keys@ pattern for JSON strings in JsonMatcher

--- src/Coduo/PHPMatcher/Matcher/JsonMatcher.php
+++ src/Coduo/PHPMatcher/Matcher/JsonMatcher.php
@@ -6,18 +6,22 @@ class JsonMatcher extends Matcher
 {
     const TRANSFORM_QUOTATION_PATTERN = '/([^"])@([a-zA-Z0-9\.]+)@([^"])/';
     const TRANSFORM_QUOTATION_REPLACEMENT = '$1"@$2@"$3';
+    const IGNORE_EXTRA_KEYS_PATTERN = "@ignore_extra_keys@";
 
     /**
      * @var
      */
     private $matcher;
 
+    private $nonStrictMatcher;
+
     /**
      * @param ValueMatcher $matcher
      */
-    public function __construct(ValueMatcher $matcher)
+    public function __construct(ValueMatcher $matcher, ValueMatcher $nonStrictMatcher = null)
     {
         $this->matcher = $matcher;
+        $this->nonStrictMatcher = $nonStrictMatcher;
     }
 
     /**
@@ -25,12 +29,16 @@ class JsonMatcher extends Matcher
      */
     public function match($value, $pattern)
     {
+        $nonStrictMatching = preg_match(self::IGNORE_EXTRA_KEYS_PATTERN, $pattern);
+
+        $pattern = str_replace(self::IGNORE_EXTRA_KEYS_PATTERN, "", $pattern);
+
         if (!is_string($value) || !$this->isValidJson($value)) {
             return false;
         }
 
         $pattern = $this->transformPattern($pattern);
-        $match = $this->matcher->match(json_decode($value, true), json_decode($pattern, true));
+        $match = $this->doMatch(json_decode($value, true), json_decode($pattern, true), $nonStrictMatching);
         if (!$match) {
             $this->error = $this->matcher->getError();
             return false;
@@ -67,6 +75,21 @@ class JsonMatcher extends Matcher
     private function transformPattern($pattern)
     {
         return preg_replace(self::TRANSFORM_QUOTATION_PATTERN, self::TRANSFORM_QUOTATION_REPLACEMENT, $pattern);
+    }
+
+    /**
+     * @param $value
+     * @param $pattern
+     * @param $matches
+     * @return bool
+     */
+    private function doMatch($value, $pattern, $nonStrictMatching)
+    {
+        if ($nonStrictMatching) {
+            return $this->nonStrictMatcher->match($value, $pattern);
+        }
+
+        return $this->matcher->match($value, $pattern);
     }
 
 }
--- tests/Coduo/PHPMatcher/Matcher/JsonMatcherTest.php
+++ tests/Coduo/PHPMatcher/Matcher/JsonMatcherTest.php
@@ -31,6 +31,9 @@ class JsonMatcherTest extends \PHPUnit_Framework_TestCase
         $this->matcher = new Matcher\JsonMatcher(new Matcher\ChainMatcher(array(
             $scalarMatchers,
             new Matcher\ArrayMatcher($scalarMatchers, $parser)
+        )), new Matcher\ChainMatcher(array(
+            $scalarMatchers,
+            new Matcher\NonStrictArrayMatcher($scalarMatchers, $parser)
         )));
     }
 
@@ -111,6 +114,14 @@ class JsonMatcherTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($this->matcher->match($value, $pattern));
 
         $this->assertEquals($this->matcher->getError(), 'There is no element under path [foo][bar][faz] in value.');
+    }
+
+    /**
+     * @dataProvider nonStrictpositiveMatches
+     */
+    public function test_non_strict_json_matching($value, $pattern)
+    {
+        $this->assertTrue($this->matcher->match($value, $pattern), $this->matcher->getError());
     }
 
     public static function positivePatterns()
@@ -228,6 +239,24 @@ class JsonMatcherTest extends \PHPUnit_Framework_TestCase
             array(
                 '{"name": "Norbert", "roles": ["ADMIN", "USER"]}',
                 '{"name": @string@, "roles": [@string@, @string@]}'
+            ),
+        );
+    }
+
+    public function nonStrictPositiveMatches()
+    {
+        return array(
+            array(
+                '{"key": "value", "foo": "bar"}',
+                '{"foo": "bar"}@ignore_extra_keys@',
+            ),
+            array(
+                '{"key": "value", "foo": "bar", "roles": ["ADMIN", "USER"], "more": {"buz": "guz", "paz": "kaz"}}',
+                '{"foo": "bar", "more": {"buz": "guz"}}@ignore_extra_keys@',
+            ),
+            array(
+                '{"key": "value", "foo": "bar", "roles": ["ADMIN", "USER"], "more": {"buz": "guz", "paz": "kaz"}}',
+                '{"foo": "bar", "more": {"buz": @string@}}@ignore_extra_keys@',
             ),
         );
     }
